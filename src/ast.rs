fc6ea2006af69ce087cbb914377dcc7d1fe93eba
Chris Gregory <czipperz@gmail.com>
2019-05-28 17:02:08 -0500

    Derive common traits for ast data

--- src/ast.rs
+++ src/ast.rs
@@ -1,13 +1,17 @@
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum TopLevel {
     Function(Function),
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Function {
     pub name: String,
     pub parameters: Vec<Parameter>,
     pub body: Vec<Statement>,
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Parameter {}
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Statement {}
