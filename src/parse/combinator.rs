3ace3a90f5671d7ca19064f41a1a8a37106cf547
Chris Gregory <czipperz@gmail.com>
2019-06-03 18:49:29 -0700

    Add a bunch of tests for combinator::many_separator

--- src/parse/combinator.rs
+++ src/parse/combinator.rs
@@ -27,7 +27,7 @@ where
 
         let old_index = parser.index;
         match separator(parser) {
-            Ok(_) => (),
+            Ok(()) => (),
             Err(_) if old_index == parser.index => return Ok(xs),
             Err(e) => return Err(e),
         }
@@ -105,6 +105,53 @@ mod tests {
                         Err(())
                     }
                 }
+            ),
+            Ok(vec![()])
+        );
+    }
+
+    #[test]
+    fn test_cant_parse_separator() {
+        assert_eq!(
+            many_separator(
+                &mut Parser::new("", &[], Pos { file: 0, index: 0 }),
+                |_| Ok(()),
+                |_| Err(())
+            ),
+            Ok(vec![()])
+        );
+    }
+
+    #[test]
+    fn test_cant_parse_separator_but_advance() {
+        assert_eq!(
+            many_separator(
+                &mut Parser::new("", &[], Pos { file: 0, index: 0 }),
+                |_| Ok(()),
+                |parser| {
+                    parser.index += 1;
+                    Err(())
+                }
+            ),
+            Err(())
+        );
+    }
+
+    #[test]
+    fn test_successful_parse_separator() {
+        let mut first = true;
+        assert_eq!(
+            many_separator(
+                &mut Parser::new("", &[], Pos { file: 0, index: 0 }),
+                |_|  {
+                    if first {
+                        first = false;
+                        Ok(())
+                    } else {
+                        Err(())
+                    }
+                },
+                |_| Ok(())
             ),
             Ok(vec![()])
         );
