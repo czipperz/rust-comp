b982ee257d9c619217f4536d660f444c67794adc
Chris Gregory <czipperz@gmail.com>
2019-06-03 21:32:31 -0700

    Rename args module to opt

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-mod args;
 mod ast;
 mod lex;
+mod opt;
 mod parse;
 mod pos;
 mod read_file;
 mod token;
 
 fn main() {
-    let args = args::parse();
-    for i in 0..args.files.len() {
-        let file_name = &args.files[i];
+    let opt = opt::parse();
+    for i in 0..opt.files.len() {
+        let file_name = &opt.files[i];
         let file_contents = read_file::read_file(file_name).expect("Cannot read input file");
         let (tokens, eofpos) = lex::read_tokens(i, &file_contents).expect("Lexing error");
         println!("{:?}", tokens);
