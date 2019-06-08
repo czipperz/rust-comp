74b5842a5c5b0489a68a2ebb9ba1ccc4618e7da5
Chris Gregory <czipperz@gmail.com>
2019-06-08 16:39:51 -0700

    Add a test to expect_type

--- src/parse/type_.rs
+++ src/parse/type_.rs
@@ -7,3 +7,19 @@ pub fn expect_type<'a>(parser: &mut Parser<'a>) -> Result<Type<'a>, Error> {
         .expect_label()
         .map(|name| Type::Named(NamedType { name }))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::lex::read_tokens;
+
+    #[test]
+    fn test_expect_type_named_type() {
+        let contents = "abc";
+        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
+        let mut parser = Parser::new(contents, &tokens, eofpos);
+        let type_ = expect_type(&mut parser).unwrap();
+        assert_eq!(parser.index, tokens.len());
+        assert_eq!(type_, Type::Named(NamedType { name: "abc" }));
+    }
+}
