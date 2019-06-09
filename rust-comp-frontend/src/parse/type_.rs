70416f876f0ba617ed57c661132045b994f28778
Chris Gregory <czipperz@gmail.com>
2019-06-09 15:20:15 -0700

    Refactor parse::type tests to use new style parse

--- rust-comp-frontend/src/parse/type_.rs
+++ rust-comp-frontend/src/parse/type_.rs
@@ -35,27 +35,23 @@ pub fn expect_type<'a>(parser: &mut Parser<'a, '_>) -> Result<Type<'a>, Error> {
 
 #[cfg(test)]
 mod tests {
+    use super::super::test::parse;
     use super::*;
-    use crate::lex::read_tokens;
     use crate::pos::Span;
 
     #[test]
     fn test_expect_type_named_type() {
-        let contents = "abc";
-        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
-        let mut parser = Parser::new(contents, &tokens, eofpos);
-        let type_ = expect_type(&mut parser).unwrap();
-        assert_eq!(parser.index, tokens.len());
+        let (index, len, type_) = parse(expect_type, "abc");
+        let type_ = type_.unwrap();
+        assert_eq!(index, len);
         assert_eq!(type_, Type::Named(NamedType { name: "abc" }));
     }
 
     #[test]
     fn test_expect_type_ref() {
-        let contents = "&abc";
-        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
-        let mut parser = Parser::new(contents, &tokens, eofpos);
-        let type_ = expect_type(&mut parser).unwrap();
-        assert_eq!(parser.index, tokens.len());
+        let (index, len, type_) = parse(expect_type, "&abc");
+        let type_ = type_.unwrap();
+        assert_eq!(index, len);
         assert_eq!(
             type_,
             Type::Ref(Box::new(Type::Named(NamedType { name: "abc" })))
@@ -64,11 +60,9 @@ mod tests {
 
     #[test]
     fn test_expect_type_ref_ref() {
-        let contents = "&&abc";
-        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
-        let mut parser = Parser::new(contents, &tokens, eofpos);
-        let type_ = expect_type(&mut parser).unwrap();
-        assert_eq!(parser.index, tokens.len());
+        let (index, len, type_) = parse(expect_type, "&&abc");
+        let type_ = type_.unwrap();
+        assert_eq!(index, len);
         assert_eq!(
             type_,
             Type::Ref(Box::new(Type::Ref(Box::new(Type::Named(NamedType {
@@ -79,11 +73,9 @@ mod tests {
 
     #[test]
     fn test_expect_type_ref_mut() {
-        let contents = "&mut abc";
-        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
-        let mut parser = Parser::new(contents, &tokens, eofpos);
-        let type_ = expect_type(&mut parser).unwrap();
-        assert_eq!(parser.index, tokens.len());
+        let (index, len, type_) = parse(expect_type, "&mut abc");
+        let type_ = type_.unwrap();
+        assert_eq!(index, len);
         assert_eq!(
             type_,
             Type::RefMut(Box::new(Type::Named(NamedType { name: "abc" })))
@@ -92,11 +84,9 @@ mod tests {
 
     #[test]
     fn test_expect_type_ptr_mut() {
-        let contents = "*mut abc";
-        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
-        let mut parser = Parser::new(contents, &tokens, eofpos);
-        let type_ = expect_type(&mut parser).unwrap();
-        assert_eq!(parser.index, tokens.len());
+        let (index, len, type_) = parse(expect_type, "*mut abc");
+        let type_ = type_.unwrap();
+        assert_eq!(index, len);
         assert_eq!(
             type_,
             Type::PtrMut(Box::new(Type::Named(NamedType { name: "abc" })))
@@ -105,11 +95,9 @@ mod tests {
 
     #[test]
     fn test_expect_type_ptr_const() {
-        let contents = "*const abc";
-        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
-        let mut parser = Parser::new(contents, &tokens, eofpos);
-        let type_ = expect_type(&mut parser).unwrap();
-        assert_eq!(parser.index, tokens.len());
+        let (index, len, type_) = parse(expect_type, "*const abc");
+        let type_ = type_.unwrap();
+        assert_eq!(index, len);
         assert_eq!(
             type_,
             Type::PtrConst(Box::new(Type::Named(NamedType { name: "abc" })))
@@ -118,13 +106,11 @@ mod tests {
 
     #[test]
     fn test_expect_type_ptr_no_qualifier() {
-        let contents = "*abc";
-        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
-        let mut parser = Parser::new(contents, &tokens, eofpos);
-        let err = expect_type(&mut parser).unwrap_err();
-        assert_eq!(parser.index, 1);
+        let (index, _, type_) = parse(expect_type, "*abc");
+        let error = type_.unwrap_err();
+        assert_eq!(index, 1);
         assert_eq!(
-            err,
+            error,
             Error::Expected(
                 "`const` or `mut` after pointer",
                 Span {
@@ -138,21 +124,17 @@ mod tests {
 
     #[test]
     fn test_expect_type_tuple_0() {
-        let contents = "()";
-        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
-        let mut parser = Parser::new(contents, &tokens, eofpos);
-        let type_ = expect_type(&mut parser).unwrap();
-        assert_eq!(parser.index, tokens.len());
+        let (index, len, type_) = parse(expect_type, "()");
+        let type_ = type_.unwrap();
+        assert_eq!(index, len);
         assert_eq!(type_, Type::Tuple(vec![]));
     }
 
     #[test]
     fn test_expect_type_tuple_1() {
-        let contents = "(abc)";
-        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
-        let mut parser = Parser::new(contents, &tokens, eofpos);
-        let type_ = expect_type(&mut parser).unwrap();
-        assert_eq!(parser.index, tokens.len());
+        let (index, len, type_) = parse(expect_type, "(abc)");
+        let type_ = type_.unwrap();
+        assert_eq!(index, len);
         assert_eq!(
             type_,
             Type::Tuple(vec![Type::Named(NamedType { name: "abc" })])
@@ -161,11 +143,9 @@ mod tests {
 
     #[test]
     fn test_expect_type_tuple_3() {
-        let contents = "(*const abc, def, &ghi)";
-        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
-        let mut parser = Parser::new(contents, &tokens, eofpos);
-        let type_ = expect_type(&mut parser).unwrap();
-        assert_eq!(parser.index, tokens.len());
+        let (index, len, type_) = parse(expect_type, "(*const abc, def, &ghi)");
+        let type_ = type_.unwrap();
+        assert_eq!(index, len);
         assert_eq!(
             type_,
             Type::Tuple(vec![
