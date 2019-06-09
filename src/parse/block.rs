167972ecb92a8a60751c9c1f7da30f95c5ba3c5c
Chris Gregory <czipperz@gmail.com>
2019-06-08 17:31:43 -0700

    Fix error handling of blocks with a final expression

--- src/parse/block.rs
+++ src/parse/block.rs
@@ -15,13 +15,25 @@ pub fn expect_block<'a>(parser: &mut Parser<'a>) -> Result<Block<'a>, Error> {
         let old_index = parser.index;
         match expect_statement(parser) {
             Ok(x) => statements.push(x),
-            Err(_e) => {
-                // TODO: The error above should be used here I don't know how
-                // right now
-                parser.index = old_index;
-                expression = maybe(parser, expect_expression)?;
+            Err(statement_err) => {
+                if old_index == parser.index {
+                    expression = maybe(parser, expect_expression)?;
+                } else {
+                    let statement_index = parser.index;
+                    parser.index = old_index;
+                    // If we cannot parse the expression, that is an error
+                    expression = Some(expect_expression(parser).map_err(|expression_err| {
+                        // If statement parsing was more successful, use its error message
+                        if parser.index < statement_index {
+                            parser.index = statement_index;
+                            statement_err
+                        } else {
+                            expression_err
+                        }
+                    })?);
+                }
                 break;
-            }
+            },
         }
     }
     parser.expect_token(TokenValue::CloseCurly)?;
@@ -35,6 +47,7 @@ pub fn expect_block<'a>(parser: &mut Parser<'a>) -> Result<Block<'a>, Error> {
 mod tests {
     use super::*;
     use crate::lex::read_tokens;
+    use crate::pos::Span;
 
     #[test]
     fn test_expect_block_no_statements() {
@@ -74,6 +87,46 @@ mod tests {
         assert_eq!(
             block.expression,
             Some(Box::new(Expression::Variable(Variable { name: "y" })))
+        );
+    }
+
+    #[test]
+    fn test_expect_block_with_statement_then_invalid_statement() {
+        let contents = "{x;let}";
+        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
+        let mut parser = Parser::new(contents, &tokens, eofpos);
+        let block = expect_block(&mut parser).unwrap_err();
+        assert_eq!(parser.index, tokens.len() - 1);
+        assert_eq!(
+            block,
+            Error::ExpectedToken(
+                TokenValue::Label,
+                Span {
+                    file: 0,
+                    start: 6,
+                    end: 7,
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn test_expect_block_with_statement_then_if_no_closing_curly() {
+        let contents = "{x;if x {";
+        let (tokens, eofpos) = read_tokens(0, contents).unwrap();
+        let mut parser = Parser::new(contents, &tokens, eofpos);
+        let block = expect_block(&mut parser).unwrap_err();
+        assert_eq!(parser.index, tokens.len());
+        assert_eq!(
+            block,
+            Error::ExpectedToken(
+                TokenValue::CloseCurly,
+                Span {
+                    file: 0,
+                    start: 9,
+                    end: 10,
+                }
+            )
         );
     }
 }
